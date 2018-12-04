3de0ee38253c5cf2997b3746b0d5e64529ee5dd8
polymath <polymath1995@gmail.com>
2018-12-04 21:29:17 +0530

    Updates

---
+++ precache-manifest.21042e6e6d9aa9d4f379f7ad61ba85ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "696538c11c773123d716",
+    "url": "/static/js/main.696538c1.chunk.js"
+  },
+  {
+    "revision": "8bc24998e9331454d5db",
+    "url": "/static/js/1.8bc24998.chunk.js"
+  },
+  {
+    "revision": "696538c11c773123d716",
+    "url": "/static/css/main.ef81d852.chunk.css"
+  },
+  {
+    "revision": "8bc24998e9331454d5db",
+    "url": "/static/css/1.6d1c3830.chunk.css"
+  },
+  {
+    "revision": "f7ea470debaa8e2c1c85e55bfca87a2b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/polymath1995.github.io/precache-manifest.024194e6294764e6a63fc043ec2d7c68.js"
+  "/precache-manifest.21042e6e6d9aa9d4f379f7ad61ba85ea.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/polymath1995.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
